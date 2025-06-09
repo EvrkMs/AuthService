a81214fb791183823b5c28cfdac667e4af0db9f1
EvrkMs <167056681+EvrkMs@users.noreply.github.com>
2025-06-09 09:24:22 +0300

    Смена email на телефон и изменение регистрации

--- src/AuthService.Application/Interfaces/IAuthService.cs
+++ src/AuthService.Application/Interfaces/IAuthService.cs
@@ -4,7 +4,7 @@ namespace AuthService.Application.Interfaces;
 public interface IAuthService
 {
     Task<(TokenResponse,string)> LoginAsync(LoginRequest req,string deviceId);
-    Task<TokenResponse> RegisterAsync(RegisterRequest req);
+    Task RegisterAsync(RegisterRequest req);
     Task<TokenResponse> RefreshAsync(string combined,string deviceId);
     Task LogoutAsync(string combined);
 }
